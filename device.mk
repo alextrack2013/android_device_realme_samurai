e44d09769e6d2891ccc74f8615dcd6baac8be082
alextrack2013 <alextrack2013@gmail.com>
2023-10-29 17:58:47 +0100

    samurai: Uprev android.hardware.bluetooth.audio to version 2.1

--- device.mk
+++ device.mk
@@ -103,7 +103,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.a2dp@1.0.vendor \
-    android.hardware.bluetooth.audio@2.0 \
+    android.hardware.bluetooth.audio@2.1 \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     audio.r_submix.default \
