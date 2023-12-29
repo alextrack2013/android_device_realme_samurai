72d0950e5392f056135258ac40d2042bab643d92
alextrack2013 <alextrack2013@gmail.com>
2023-12-29 20:41:21 +0100

    tmp
    Change-Id: Id3ecdb1d339c903e59ce093ecf75ce42e58afbda

--- device.mk
+++ device.mk
@@ -396,7 +396,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr
+    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
