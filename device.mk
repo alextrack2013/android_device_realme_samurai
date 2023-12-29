cb51dbe84cb38135e2ee869230876db0094aa3d6
alextrack2013 <alextrack2013@gmail.com>
2023-12-29 22:12:40 +0100

    fixup! samurai: Disable memtrack support
    * Seems like it wasn't fully nuked
    
    Change-Id: I53ff084fa65a4ecb332816beda26e7899e9c5141

--- device.mk
+++ device.mk
@@ -191,8 +191,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     disable_configstore \
     gralloc.msmnile \
     hwcomposer.msmnile \
@@ -201,7 +199,6 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libtinyxml \
     libvulkan \
-    memtrack.msmnile \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
